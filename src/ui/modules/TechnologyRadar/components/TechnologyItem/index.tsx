70377c7f4499ab3702c8a3e761a887784fdaa3d2
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2018-05-13 19:18:13 +0200

    refactor: Separates item calculations into separate methods.

--- src/ui/modules/TechnologyRadar/components/TechnologyItem/index.tsx
+++ src/ui/modules/TechnologyRadar/components/TechnologyItem/index.tsx
@@ -50,51 +50,80 @@ export class TechnologyItem extends Component<TechnologyItemProps> {
   // ----------------------------------------------------------------------------- Helpers methods
   private calculateTransforms(props: TechnologyItemProps): { transform: string, width: string} {
     const { technology, technologies, groups, group, settings } = props;
+    const { innerRadiusPercent, outerRadiusPercent } = settings;
 
-    const maxLevel = this.getMaxLevel(technologies);
+    // The index of the item's group within the dataset's groups
     const groupIndex = groups.findIndex(acc => acc.id === group.id);
 
-    const radiusPercent = settings.outerRadiusPercent;
-
-    const level = (maxLevel + 1) - technology.level;
-
-    const index = this.calculateIndexForLevelAndGroup(technologies, technology);
+    // Calculate number of items and the current item's index only in relation
+    // to their current group and level (i.e., disregard other item's within that group / level).
     const count = this.calculateNumberOfItemsForLevelAndGroup(technologies, technology);
+    const index = this.calculateIndexForLevelAndGroup(technologies, technology);
 
+    // The highest level set in the dataset
+    const maxLevel = this.getMaxLevel(technologies);
+
+    const rotationDegrees = this.calculateItemRotationDegrees({
+      groupIndex,
+      groupSize: groups.length,
+      count,
+      index
+    });
+
+    const translationPercent = this.calculateItemTranslationPercent({
+      index,
+      count,
+      level: technology.level,
+      maxLevel,
+      innerRadiusPercent,
+      outerRadiusPercent
+    });
+
+    return {
+      transform: `rotateZ(${ rotationDegrees }deg)`,
+      width: `${ translationPercent }%`,
+    };
+  }
+
+  private calculateItemRotationDegrees(rotationProperties: ItemRotationProperties): number {
+    const { groupIndex, groupSize, count, index } = rotationProperties;
+    // Rotate item to it's group
+    //
     // Angle for each group segment in the dataset.
-    const groupBaseAngleDegree = 360 / groups.length;
+    const groupBaseAngleDegree = 360 / groupSize;
     // Angle for each item in the item's level range within that group.
     const itemBaseAngleDegree = groupBaseAngleDegree / (count + 1);
     // Rotate to fit item into parent group.
     const groupRotationDegree = groupBaseAngleDegree + groupIndex * groupBaseAngleDegree;
     // Rotate item within group
     const itemRotationDegree = index * itemBaseAngleDegree + 0.5 * itemBaseAngleDegree;
-    // Final rotation calculated by adding total rotation base on item-group
-    // and item rotation within group.
-    const rotationDegree = groupRotationDegree + itemRotationDegree + 0.5 * itemBaseAngleDegree;
+    //
+    // Final rotation
+    //
+    return groupRotationDegree + itemRotationDegree + 0.5 * itemBaseAngleDegree;
+  }
 
-    console.log('+++ count', count);
+  private calculateItemTranslationPercent(transformProperties: ItemTransformProperties): number {
+    const { index, count, level, maxLevel, innerRadiusPercent, outerRadiusPercent } = transformProperties;
 
-    // Adds shift in translation to prevent overlap between neighbouring items.
-    const levelRangeRatio = 1 / maxLevel;
-    // Shift to middle of current level
-    const offsetPercent = settings.innerRadiusPercent + levelRangeRatio * 0.5 * radiusPercent;
-    // Calculate base translation for each level
-    const levelBaseTranslationPercent = levelRangeRatio * (settings.outerRadiusPercent - settings.innerRadiusPercent);
-    // Add some variation with alternating between -1, 0, 1 to keep items from overlapping
+    // Invert level, i.e., the greater the level the nearer the item is to the center
+    const invertedLevel = (maxLevel + 1) - level;
+
+    // Translate item to it's level
+    //
+    // Calculate base translation for each level between inner and outer radius
+    const levelBaseTranslationPercent = (outerRadiusPercent - innerRadiusPercent) / maxLevel;
+    // Add base shift to current item's level
+    const levelShiftPercent = (invertedLevel - 1) * levelBaseTranslationPercent;
+    // Add shift item to current item's level center
+    const centerShiftPercent = 0.5 * levelBaseTranslationPercent;
+    // Add some variation with alternating between [-1, 1] to keep items from overlapping
     const variation = count < 2 ? 0 : (index % 2 === 0 ? 1 : -1);
-    const translationVariationPercent = variation * 0.25 * offsetPercent;
-
-  console.log('+++ o', level, levelBaseTranslationPercent, settings.innerRadiusPercent);
-
-    const translationPercent = settings.innerRadiusPercent + (level - 1) * levelBaseTranslationPercent + 0.5 * levelBaseTranslationPercent + translationVariationPercent;
-
-    return {
-      transform: [
-        `rotate(${rotationDegree}deg)`
-      ].join(' '),
-      width: `${translationPercent}%`,
-    };
+    const translationVariationPercent = variation * 0.25 * centerShiftPercent;
+    //
+    // Final translation
+    //
+    return innerRadiusPercent + levelShiftPercent + centerShiftPercent + translationVariationPercent;
   }
 
   private calculateNumberOfItemsForLevelAndGroup(technologies: Technology[], technology: Technology): number {
---
+++ src/ui/modules/TechnologyRadar/components/TechnologyItem/typings.d.ts
@@ -0,0 +1,15 @@
+declare interface ItemTransformProperties {
+    index: number;
+    count: number;
+    level: number;
+    maxLevel: number;
+    innerRadiusPercent: number;
+    outerRadiusPercent: number;
+  }
+
+  declare interface ItemRotationProperties {
+    groupIndex: number;
+    groupSize: number;
+    count: number;
+    index: number;
+  }
